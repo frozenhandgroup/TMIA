c9c34fa1ab31b7c154b5a01603956fc3bf0dd755
lioz3190 <ddavid3190@gmail.com>
2016-02-09 16:59:38 +0100

    tout a fait
    gogol

--- Nouveau document texte.java
+++ Nouveau document texte.java
@@ -1,5 +1,5 @@
 public void main(){
 	system.out.println("Taaronne");
 	system.out.printl("et ta soeur");
-	system.out.println("bande de trilobite");
+	system.out.println("bande de trilobite en patte");
 	}
